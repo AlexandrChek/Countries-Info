b71aff9b2cf38500a325d2b7e0e7ee07b4cba616
Chekcheev_Alexandr <chekcheev.alex@gmail.com>
2024-09-12 22:28:14 +0300

    1.1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AppRoutes from './AppRoutes'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
+  console.log(process.env.VITE_BASE_URL)
   return (
     <Router>
       <AppRoutes/>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const BASE = process.env.VITE_BASE_URL
 
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === 'development' ? '/' : '/Countries-Info/'
+    base: mode === 'development' ? '/' : BASE
   }
 })
