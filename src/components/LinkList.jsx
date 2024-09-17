1aa9e3625b084612f2e855e230aec2eecc7c9232
Chekcheev_Alexandr <chekcheev.alex@gmail.com>
2024-09-17 18:42:38 +0300

    link styles corrected

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -4,7 +4,7 @@ const LinkList = ({ countries, ...rest }) => {
   return (
     <ul {...rest}>
       {countries.map((country) => (
-        <li key={country.countryCode}>
+        <li key={country.countryCode} style={{marginBottom: '2.5px'}}>
           <CountryLink to={`/country/${country.countryCode}`} state={country}>
             {country.name}
           </CountryLink>
