59bba0e463324805e4e190a71649d82b63207130
Chekcheev_Alexandr <chekcheev.alex@gmail.com>
2024-09-21 18:17:34 +0300

    hook useSetMinHeight sdded

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
+import useSetMinHeight from './hooks/useSetMinHeight';
 import AppRoutes from './AppRoutes';
 import HomeLink from './components/HomeLink';
 
 function App() {
-  const setHeight = () => {
-    const vh = window.innerHeight;
-    document.documentElement.style.setProperty('--vh', `${vh}`);
-  };
-
-  if (window.matchMedia('(hover: none) and (pointer: coarse)').matches) {
-    window.addEventListener('load', setHeight);
-    window.addEventListener('resize', setHeight);
-  }
+  useSetMinHeight();
 
   return (
     <>
---
+++ src/hooks/useSetMinHeight.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+const useSetMinHeight = () => {
+  useEffect(() => {
+    const setMinHeight = () => {
+      const vh = window.innerHeight;
+      document.documentElement.style.setProperty('--vh', `${vh}`);
+    };
+
+    if (window.matchMedia('(hover: none) and (pointer: coarse)').matches) {
+      window.addEventListener('load', setMinHeight);
+      window.addEventListener('resize', setMinHeight);
+    }
+
+    return () => {
+      window.removeEventListener('load', setMinHeight);
+      window.removeEventListener('resize', setMinHeight);
+    };
+  }, []);
+};
+
+export default useSetMinHeight;
